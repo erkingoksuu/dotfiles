0a5a8c55a84f075ba42c462e336e81525ebe073f
erkingoksuu <erkingoksuu@gmail.com>
2024-12-03 04:06:02 +0300

    last

--- lib/install/dotfiles/reboot.sh
+++ lib/install/dotfiles/reboot.sh
@@ -9,10 +9,8 @@ if gum confirm "Do you want to reboot your system now?" ;then
     gum spin --spinner dot --title "Rebooting now..." -- sleep 3
     systemctl reboot
 elif [ $? -eq 130 ]; then
-    _writeCancel
     exit 130
 else
-    _writeCancel
     exit 130
 fi
 echo
